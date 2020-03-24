378119549cb803608c911a64ee380693a14ad1b1
kyle <44934418+kwsantiago@users.noreply.github.com>
2020-03-24 18:42:26 -0400

    Add POST

--- Enpoint/falcon_endpoint.py
+++ Enpoint/falcon_endpoint.py
@@ -4,6 +4,9 @@ class NamesResource(object):
     names = [{"id": 1, "name": "Kyle Santiago"}, {"id": 2, "name": "Sandy Sue"}]
     def on_get(self, req, resp):
         resp.body = json.dumps(self.names)
+    def on_post(self, req, resp):
+        resp.status = falcon.HTTP_201
+        resp.body = json.dumps({"success": True})
 
 api = falcon.API()
 names_endpoint = NamesResource()
