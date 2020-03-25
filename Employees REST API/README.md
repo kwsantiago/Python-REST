e0686fc5c8efa9ebe6410b83882e2ed400367e24
Kyle Santiago <kwsantiago@mail.usf.edu>
2020-03-25 00:21:42 -0400

    Move to folder

---
+++ Employees REST API/README.md
@@ -0,0 +1,1 @@
+DB was taken from [here](https://www.sqlitetutorial.net/sqlite-sample-database/).
---
+++ Employees REST API/server.py
@@ -0,0 +1,36 @@
+from flask import Flask, request
+from flask_jsonpify import jsonify
+from flask_restful import Resource, Api
+from sqlalchemy import create_engine
+from json import dumps
+
+db_connect = create_engine('sqlite://chinook.db')
+app = Flask(__name__)
+api = Api(app)
+
+class Employees(Resource):
+    def get(self):
+        conn = db_connect.connect() # connect to the DB
+        query = conn.execute("select * from employees") # executes SQL query and returns result in JSON
+        return {'employees': [i[0] for i in query.cursor.fetchall()]} # fetches first column that is Employee ID
+
+class Tracks(Resource):
+    def get(self):
+        conn = db_connect.connect() # connect to the DB
+        query = conn.execute("select trackid, name, composer, unitprice from tracks") # executes SQL query and returns result in JSON
+        result = {'data': [dict(zip(tuple (query.keys()), i)) for i in query.cursor]} # fetches first column that is Employee ID
+        return jsonify(result)
+
+class Employee_Name(Resource):
+    def get(self, employee_id):
+        conn = db_connect.connect() # connect to the DB
+        query = conn.execute("select * from employees where EmployeeID = %d " %int(employee_id)) # executes SQL query and returns result in JSON
+        result = {'data': [dict(zip(tuple (query.keys()), i)) for i in query.cursor]} # fetches first column that is Employee ID
+        return jsonify(result)
+
+api.add_resource(Employees, '/employees') # Route 1
+api.add_resource(Tracks, '/tracks') # Route 2
+api.add_resource(Employee_Name, '/employees/<employee_id>') # Route 3
+
+if __name__ == '__main__':
+    app.run(port='5002')
