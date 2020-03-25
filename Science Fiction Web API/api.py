6dbbebe799f2711ebfc878caedd991bd38e4df2a
kyle <44934418+kwsantiago@users.noreply.github.com>
2020-03-24 20:03:03 -0400

    Add books and GET method for it

--- Science Fiction Web API/api.py
+++ Science Fiction Web API/api.py
@@ -1,9 +1,33 @@
 import flask
+from flask import request, jsonify
 
 app = flask.Flask(__name__)
 app.config["DEBUG"] = True
 
+books = [
+    {'id': 0,
+     'title': 'The Hobbit',
+     'author': 'J.R.R. Tolkien',
+     'first_sentence': 'Not a nasty, dirty, wet hole, filled with the ends of worms and an oozy smell, nor yet a dry, bare, sandy hole with nothing in it to sit down on or to eat: it was a hobbit-hole, and that means comfort.',
+     'year_published': '1937'},
+    {'id': 1,
+     'title': 'The Ones Who Walk Away From Omelas',
+     'author': 'Ursula K. Le Guin',
+     'first_sentence': 'With a clamor of bells that set the swallows soaring, the Festival of Summer came to the city Omelas, bright-towered by the sea.',
+     'published': '1973'},
+    {'id': 2,
+     'title': 'Dhalgren',
+     'author': 'Samuel R. Delany',
+     'first_sentence': 'to wound the autumnal city.',
+     'published': '1975'}
+]
+
 @app.route('/', methods=['GET'])
 def home():
     return "<h1>Distant Reading Archive</h1><p>This site is a prototype API for distant reading of science fiction novels.</p>"
+
+@app.route('/api/resources/books/all', methods=['GET'])
+def api_all():
+    return jsonify(books)
+
 app.run()
