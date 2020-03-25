85e388bd958b27cf1ec0c5f6cd986a12081b81c8
kyle <44934418+kwsantiago@users.noreply.github.com>
2020-03-24 20:09:49 -0400

    Change HTTP output

--- Science Fiction Web API/api.py
+++ Science Fiction Web API/api.py
@@ -24,7 +24,7 @@ books = [
 
 @app.route('/', methods=['GET'])
 def home():
-    return "<h1>Distant Reading Archive</h1><p>This site is a prototype API for distant reading of science fiction novels.</p>"
+    return "<h1>Reading Archive</h1><p>This site is a prototype API for the novels.</p>"
 
 @app.route('/api/resources/books/all', methods=['GET'])
 def api_all():
